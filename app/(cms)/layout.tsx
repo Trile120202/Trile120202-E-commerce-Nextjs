4369ce26516200501788da8fee7c430120e79b93
LongDevLor <tranthoilong@gmail.com>
2024-10-29 10:18:49 +0700

    UI

--- app/(cms)/layout.tsx
+++ app/(cms)/layout.tsx
@@ -24,56 +24,79 @@ const Sidebar = () => {
                 <nav>
                     <ul>
                         <li className="mb-4">
-                            <Link href="/quan-tri" className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
-                                <FaHome className="mr-3" />
+                            <Link href="/quan-tri"
+                                  className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
+                                <FaHome className="mr-3"/>
                                 Dashboard
                             </Link>
                         </li>
                         <li className="mb-4">
-                            <Link href="/quan-tri/quan-ly-san-pham" className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
-                                <FaBox className="mr-3" />
+                            <Link href="/quan-tri/quan-ly-san-pham"
+                                  className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
+                                <FaBox className="mr-3"/>
                                 Quản lý sản phẩm
                             </Link>
                         </li>
                         <li className="mb-4">
-                            <Link href="/quan-tri/quan-ly-loai-san-pham" className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
-                                <FaList className="mr-3" />
+                            <Link href="/quan-tri/quan-ly-loai-san-pham"
+                                  className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
+                                <FaList className="mr-3"/>
                                 Quản lý loại sản phẩm
                             </Link>
                         </li>
                         <li className="mb-4">
-                            <Link href="/quan-tri/quan-ly-tu-khoa" className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
-                                <FaTags className="mr-3" />
+                            <Link href="/quan-tri/quan-ly-tu-khoa"
+                                  className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
+                                <FaTags className="mr-3"/>
                                 Quản lý từ khóa
                             </Link>
                         </li>
                         <li className="mb-4">
-                            <Link href="/quan-tri/quan-ly-media" className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
-                                <FaImage className="mr-3" />
+                            <Link href="/quan-tri/quan-ly-media"
+                                  className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
+                                <FaImage className="mr-3"/>
                                 Quản lý media
                             </Link>
                         </li>
                         <li className="mb-4">
-                            <Link href="/quan-tri/quan-ly-don-hang" className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
-                                <FaShoppingCart className="mr-3" />
+                            <Link href="/quan-tri/ram"
+                                  className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
+                                <FaImage className="mr-3"/>
+                                Quản lý ram
+                            </Link>
+                        </li>
+                        <li className="mb-4">
+                            <Link href="/quan-tri/quan-ly-o-cung"
+                                  className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
+                                <FaImage className="mr-3"/>
+                                Quản lý ổ cứng
+                            </Link>
+                        </li>
+                        <li className="mb-4">
+                            <Link href="/quan-tri/quan-ly-don-hang"
+                                  className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
+                                <FaShoppingCart className="mr-3"/>
                                 Quản lý đơn hàng
                             </Link>
                         </li>
                         <li className="mb-4">
-                            <Link href="/quan-tri/quan-ly-banner" className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
-                                <FaImages className="mr-3" />
+                            <Link href="/quan-tri/quan-ly-banner"
+                                  className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
+                                <FaImages className="mr-3"/>
                                 Quản lý banner
                             </Link>
                         </li>
                         <li className="mb-4">
-                            <Link href="/quan-tri/quan-ly-nguoi-dung" className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
-                                <FaUsers className="mr-3" />
+                            <Link href="/quan-tri/quan-ly-nguoi-dung"
+                                  className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
+                                <FaUsers className="mr-3"/>
                                 Quản lý người dùng
                             </Link>
                         </li>
                         <li className="mb-4">
-                            <Link href="/quan-tri/cai-dat" className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
-                                <FaCog className="mr-3" />
+                            <Link href="/quan-tri/cai-dat"
+                                  className="flex items-center p-2 hover:bg-gray-700 rounded text-sm lg:text-base">
+                                <FaCog className="mr-3"/>
                                 Cài đặt
                             </Link>
                         </li>
@@ -85,13 +108,13 @@ const Sidebar = () => {
 };
 
 const Layout = ({
-    children,
-}: Readonly<{
+                    children,
+                }: Readonly<{
     children: React.ReactNode;
 }>) => {
     return (
         <div className="flex h-screen bg-gray-100">
-            <Sidebar />
+            <Sidebar/>
             <div className="flex-1 flex flex-col overflow-hidden">
                 <header className="bg-white shadow-md p-4 pl-16 lg:pl-4">
                     <h1 className="text-xl lg:text-2xl font-bold">CMS Dashboard</h1>
