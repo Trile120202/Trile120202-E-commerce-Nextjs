aedc47accb7ec885409ba8906a60ab38bc971ad6
LongDevLor <tranthoilong@gmail.com>
2024-11-18 16:49:21 +0700

    chi tiết sản phẩm

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,24 @@
 const nextConfig = {
     images: {
         domains: ['picsum.photos','utfs.io'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+            },
+        ],
       },
     eslint: {
         ignoreDuringBuilds: true,
     },
     experimental: {
-        serverComponentsExternalPackages: ['knex']
+        serverComponentsExternalPackages: ['knex'],
+        optimizeCss: false,
     },
+
+
+
+
 };
 
 export default nextConfig;
